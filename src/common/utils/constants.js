a6250a1d98eeb581d13209a3157784b02119e914
CrankySupertoon <CrankySupertoon@gmail.com>
2020-10-09 20:11:14 -0400

    fix legacy java fixer url

--- src/common/utils/constants.js
+++ src/common/utils/constants.js
@@ -1,23 +1,25 @@
 export const MC_MANIFEST_URL =
-  'https://launchermeta.mojang.com/mc/game/version_manifest.json';
-export const MC_RESOURCES_URL = 'http://resources.download.minecraft.net';
-export const FABRIC_APIS = 'https://meta.fabricmc.net/v2';
+  "https://launchermeta.mojang.com/mc/game/version_manifest.json";
+export const MC_RESOURCES_URL = "http://resources.download.minecraft.net";
+export const FABRIC_APIS = "https://meta.fabricmc.net/v2";
 export const LEGACYJAVAFIXER_URL =
-  'https://cdn.crankysupertoon.live/fmllibskoala/legacyjavafixer-1.0.jar';
-export const MOJANG_APIS = 'https://authserver.mojang.com';
-export const JAVA_MANIFEST_URL = 'https://api.adoptopenjdk.net/v2/latestAssets/releases/openjdk8?arch=x64&heap_size=normal&openjdk_impl=hotspot&type=jre';
-export const MAVEN_REPO = 'http://central.maven.org/maven2';
-export const MC_LIBRARIES_URL = 'https://libraries.minecraft.net';
-export const FORGESVC_URL = 'https://addons-ecs.forgesvc.net/api/v2';
+  "https://cdn.crankysupertoon.live/koala/fmllibs/legacyjavafixer-1.0.jar";
+export const MOJANG_APIS = "https://authserver.mojang.com";
+export const JAVA_MANIFEST_URL =
+  "https://api.adoptopenjdk.net/v2/latestAssets/releases/openjdk8?arch=x64&heap_size=normal&openjdk_impl=hotspot&type=jre";
+export const MAVEN_REPO = "http://central.maven.org/maven2";
+export const MC_LIBRARIES_URL = "https://libraries.minecraft.net";
+export const FORGESVC_URL = "https://addons-ecs.forgesvc.net/api/v2";
 export const FORGESVC_CATEGORIES =
-  'https://addons-ecs.forgesvc.net/api/v2/category?gameId=432';
+  "https://addons-ecs.forgesvc.net/api/v2/category?gameId=432";
 export const NEWS_URL =
-  'https://www.minecraft.net/content/minecraft-net/_jcr_content.articles.grid?tileselection=auto&tagsPath=minecraft:article/news,minecraft:article/insider,minecraft:article/culture,minecraft:article/merch,minecraft:stockholm/news,minecraft:stockholm/guides,minecraft:stockholm/events,minecraft:stockholm/minecraft-builds,minecraft:stockholm/marketplace,minecraft:stockholm/deep-dives,minecraft:stockholm/merch,minecraft:stockholm/earth,minecraft:stockholm/dungeons,minecraft:stockholm/realms-plus,minecraft:stockholm/minecraft,minecraft:stockholm/realms-java,minecraft:stockholm/nether&propResPath=/content/minecraft-net/language-masters/it-it/jcr:content/root/generic-container/par/bleeding_page_sectio_1278766118/page-section-par/grid&count=500&pageSize=10&lang=/content/minecraft-net/language-masters/en-us';
-export const FMLLIBS_OUR_BASE_URL = 'https://cdn.crankysupertoon.live/koala/fmllibs';
+  "https://www.minecraft.net/content/minecraft-net/_jcr_content.articles.grid?tileselection=auto&tagsPath=minecraft:article/news,minecraft:article/insider,minecraft:article/culture,minecraft:article/merch,minecraft:stockholm/news,minecraft:stockholm/guides,minecraft:stockholm/events,minecraft:stockholm/minecraft-builds,minecraft:stockholm/marketplace,minecraft:stockholm/deep-dives,minecraft:stockholm/merch,minecraft:stockholm/earth,minecraft:stockholm/dungeons,minecraft:stockholm/realms-plus,minecraft:stockholm/minecraft,minecraft:stockholm/realms-java,minecraft:stockholm/nether&propResPath=/content/minecraft-net/language-masters/it-it/jcr:content/root/generic-container/par/bleeding_page_sectio_1278766118/page-section-par/grid&count=500&pageSize=10&lang=/content/minecraft-net/language-masters/en-us";
+export const FMLLIBS_OUR_BASE_URL =
+  "https://cdn.crankysupertoon.live/koala/fmllibs";
 export const FMLLIBS_FORGE_BASE_URL =
-  'https://files.minecraftforge.net/fmllibs';
-export const IMGUR_CLIENT_ID = '509a564cf4466c9';
-export const FABRIC = 'fabric';
-export const FORGE = 'forge';
-export const VANILLA = 'vanilla';
-export const CURSEFORGE_URL = 'https://www.curseforge.com';
+  "https://files.minecraftforge.net/fmllibs";
+export const IMGUR_CLIENT_ID = "509a564cf4466c9";
+export const FABRIC = "fabric";
+export const FORGE = "forge";
+export const VANILLA = "vanilla";
+export const CURSEFORGE_URL = "https://www.curseforge.com";
