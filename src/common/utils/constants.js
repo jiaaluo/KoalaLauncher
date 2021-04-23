2106c17d63933bfd844229a38ac1f709da86f25d
Nora Hanegan <norahanegan@gmail.com>
2021-04-23 18:59:37 -0400

    constants: cleanup

--- src/common/utils/constants.js
+++ src/common/utils/constants.js
@@ -1,34 +1,58 @@
+// Java Manifest
+export const MAVEN_REPO = 'http://central.maven.org/maven2';
+export const JAVA_MANIFEST_URL = 'https://cdn.assets-gdevs.com/openjdk8.json';
+export const MC_LIBRARIES_URL = 'https://libraries.minecraft.net';
+export const LJF_URL =
+  'https://dist.creeper.host/FTB2/maven/net/minecraftforge/lex/legacyjavafixer/1.0/legacyjavafixer-1.0.jar';
+
+// FTB API
+export const FTB_API_URL = 'https://api.modpacks.ch/public';
+export const FTB_MODPACK_URL = 'https://feed-the-beast.com/modpack';
+
+// CurseForge API
+export const CURSEFORGE_URL = 'https://www.curseforge.com';
+export const FORGESVC_URL = 'https://addons-ecs.forgesvc.net/api/v2';
+export const FORGESVC_CATEGORIES =
+  'https://addons-ecs.forgesvc.net/api/v2/category?gameId=432';
+
+// Minecraft API
 export const MC_MANIFEST_URL =
   'https://launchermeta.mojang.com/mc/game/version_manifest.json';
 export const MC_RESOURCES_URL = 'http://resources.download.minecraft.net';
-export const FABRIC_APIS = 'https://meta.fabricmc.net/v2';
-export const GDL_LEGACYJAVAFIXER_MOD_URL =
-  'https://gdevs.io/legacyjavafixer-1.0.jar';
-export const MOJANG_APIS = 'https://authserver.mojang.com';
-export const JAVA_MANIFEST_URL = 'https://cdn.assets-gdevs.com/openjdk8.json';
-export const MAVEN_REPO = 'http://central.maven.org/maven2';
-export const MC_LIBRARIES_URL = 'https://libraries.minecraft.net';
-export const FORGESVC_URL = 'https://addons-ecs.forgesvc.net/api/v2';
-export const FTB_API_URL = 'https://api.modpacks.ch/public';
-export const FTB_MODPACK_URL = 'https://feed-the-beast.com/modpack';
-export const FORGESVC_CATEGORIES =
-  'https://addons-ecs.forgesvc.net/api/v2/category?gameId=432';
+
+// Minecraft News
 export const NEWS_URL =
   'https://www.minecraft.net/en-us/feeds/community-content/rss';
+
+// Forge APIs
 export const FMLLIBS_OUR_BASE_URL = 'https://fmllibs.gdevs.io';
 export const FMLLIBS_FORGE_BASE_URL =
   'https://files.minecraftforge.net/fmllibs';
-export const IMGUR_CLIENT_ID = '509a564cf4466c9';
+
+// Fabric APIs
+export const FABRIC_APIS = 'https://meta.fabricmc.net/v2';
+
+// Mod Loader Type
 export const FABRIC = 'fabric';
 export const FORGE = 'forge';
 export const VANILLA = 'vanilla';
 
+// Sources
 export const CURSEFORGE = 'curseforge';
 export const FTB = 'ftb';
 
+// API Keys
+export const IMGUR_API_KEY = '509a564cf4466c9';
+export const PASTEBIN_API_KEY = '478d52a29c7e952ba116d09bd9625fde';
+
+// Account Types
 export const ACCOUNT_MOJANG = 'ACCOUNT_MOJANG';
 export const ACCOUNT_MICROSOFT = 'ACCOUNT_MICROSOFT';
-export const CURSEFORGE_URL = 'https://www.curseforge.com';
+
+// Mojang Account Auth
+export const MOJANG_APIS = 'https://authserver.mojang.com';
+
+// Microsoft Account Auth
 export const MICROSOFT_LIVE_LOGIN_URL = 'https://login.live.com';
 export const MICROSOFT_XBOX_LOGIN_URL = 'https://user.auth.xboxlive.com';
 export const MICROSOFT_XSTS_AUTH_URL = 'https://xsts.auth.xboxlive.com';
